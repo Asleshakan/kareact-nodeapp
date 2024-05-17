dee651127f284f0c3d057eeb91e21c512cd4de01
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-05-17 10:09:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const path = require('path'); // Make sure path is required
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,15 +10,14 @@ app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
-  });
-  
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+});
 
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
-  });
+});
